e8714e73d1db7a72aec8da9096c57b74b8c69059
hmym <chika.hamayama@gmail.com>
2019-08-05 21:27:21 +0900

    scheduledTimer追加

--- testBar/FirstViewController.swift
+++ testBar/FirstViewController.swift
@@ -8,13 +8,27 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
 
+class FirstViewController: UIViewController {
+//    var countNumber: Int = 0
+    @IBOutlet weak var timeText: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        timeText.text = "TODAY"
+        Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(FirstViewController.timerUpdate), userInfo: nil, repeats: true)
     }
+    @objc func timerUpdate() {
+    
+//        countNumber += 1
+        let date: Date = Date()
+        let format = DateFormatter()
+        format.dateFormat = "yyyy/MM/dd HH:mm:ss"
+        timeText.text = format.string(from: date)
+        
 
+//      timeText.text = Date.stringFromDate(date: Date(), format: "yyyy/MM/dd HH:mm:ss")
+    }
 
 }
 
